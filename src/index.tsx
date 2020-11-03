202c7f6832ed7dbcf177f75c7025c2a838a1731c
davidhojman <43155033+davidhojman@users.noreply.github.com>
2020-11-03 10:43:53 -0300

    mobile view (#10)

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
-import {store} from 'config/store'
+import { store } from 'config/store'
 
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
+import Media from 'react-media';
 
-axios.defaults.baseURL= process.env.REACT_APP_API_BASE_URL
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
 axios.interceptors.request.use((config) => {
     config.params = config.params || {};
     config.params['token'] = process.env.REACT_APP_ARCHILOGIC_PUBLISHABLE_API_KEY;
@@ -18,12 +19,18 @@ axios.interceptors.request.use((config) => {
     return Promise.reject(error);
 });
 
-
-
-
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Media query="(max-width: 500px)" render={() =>
+            (
+                <div><img className='device-not-supported' src={require("./warning.png")} alt="" /></div>
+            )}
+        />
+        <Media query="(min-width: 501px)" render={() =>
+            (
+                <App />
+            )}
+        />
     </Provider>,
     document.getElementById('root'));
 
